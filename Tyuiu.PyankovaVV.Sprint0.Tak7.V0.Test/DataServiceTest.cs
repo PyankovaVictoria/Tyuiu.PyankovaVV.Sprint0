ba142eb89ff0ea491dc18b14937c4d36c6bc5753
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-09-16 22:58:43 +0500

    Task7

--- Tyuiu.PyankovaVV.Sprint0.Tak7.V0.Test/DataServiceTest.cs
+++ Tyuiu.PyankovaVV.Sprint0.Tak7.V0.Test/DataServiceTest.cs
@@ -3,7 +3,7 @@ using System;
 
 using Tyuiu.PyankovaVV.Sprint0.Task7.V0.Lib;
 
-namespace Tyuiu.PyankovaVV.Sprint0.Tak7.V0.Test
+namespace Tyuiu.PyankovaVV.Sprint0.Task7.V0.Test
 {
     [TestClass]
     public class DataServiceTest

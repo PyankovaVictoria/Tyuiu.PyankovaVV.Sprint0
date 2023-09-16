395f520ee796f27f16fbb42c6f92fc1cb97eff6f
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-09-16 22:48:26 +0500

    Task4

--- Tyuiu.PyankovaVV.Sprint0.Task4.V0.Lib/DataService.cs
+++ Tyuiu.PyankovaVV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Tyuiu.PyankovaVV.Sprint0.Task4.V0.Lib
 {
     public class DataService
